6df9bbc0d85180fd853c606f25b4c0d2dfb04945
Itay Dafna <itaydafna@gmail.com>
2019-06-12 23:59:15 +0300

    optimize gameBoard reducer

--- src/reducers/gameBoard.js
+++ src/reducers/gameBoard.js
@@ -9,6 +9,30 @@ const initialState = {
 	squares: [],
 };
 
+// squares reducer TODO: consider moving to a sperate module
+function squares(state = initialState.squares, action) {
+	switch (action.type) {
+		case ACTIONS.INIT_GAME_BOARD:
+			return [...new Array(action.payload.rows)].map(row =>
+				[...new Array(action.payload.columns)].map(col => square(undefined, action))
+			);
+		case ACTIONS.ALLOCATE_BOMB:
+		case ACTIONS.ALLOCATE_ADJACENT_BOMBS:
+		case ACTIONS.REVEAL_SQUARE:
+			return [
+				...state.slice(0, action.payload.row),
+				[
+					...state[action.payload.row].slice(0, action.payload.column),
+					square(state[action.payload.row][action.payload.column], action),
+					...state[action.payload.row].slice(action.payload.column + 1),
+				],
+				...state.slice(action.payload.row + 1),
+			];
+		default:
+			return state;
+	}
+}
+
 export default function gameBoard(state = initialState, action) {
 	switch (action.type) {
 		case ACTIONS.INIT_GAME_BOARD:
@@ -17,37 +41,20 @@ export default function gameBoard(state = initialState, action) {
 				columns: action.payload.columns,
 				rows: action.payload.rows,
 				bombs: action.payload.bombs,
-				squares: [...new Array(action.payload.rows)].map(row => [...new Array(action.payload.columns)].map(col => square(undefined, action))),
-      };
-      
+				squares: squares(state.squares,action),
+			};
+
 		case ACTIONS.ALLOCATE_BOMB:
 		case ACTIONS.ALLOCATE_ADJACENT_BOMBS:
 			return {
 				...state,
-				squares: [
-					...state.squares.slice(0, action.payload.row),
-					[
-						...state.squares[action.payload.row].slice(0, action.payload.column),
-						square(state.squares[action.payload.row][action.payload.column], action),
-						...state.squares[action.payload.row].slice(action.payload.column + 1),
-					],
-					...state.squares.slice(action.payload.row + 1),
-				]
-      };
-      case ACTIONS.REVEAL_SQUARE:
-          return {
-						...state,
-						isFirstRevealed: true,
-            squares: [
-              ...state.squares.slice(0, action.payload.row),
-              [
-                ...state.squares[action.payload.row].slice(0, action.payload.column),
-                square(state.squares[action.payload.row][action.payload.column], action),
-                ...state.squares[action.payload.row].slice(action.payload.column + 1),
-              ],
-              ...state.squares.slice(action.payload.row + 1),
-            ]
-          };
+				squares:  squares(state.squares,action)}
+		case ACTIONS.REVEAL_SQUARE:
+			return {
+				...state,
+				isFirstRevealed: true,
+				squares:  squares(state.squares,action)
+			};
 
 		default:
 			return state;
