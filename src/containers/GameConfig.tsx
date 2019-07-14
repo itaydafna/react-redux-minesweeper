7ba2ae376baf0dda020611eee99290404d1bb2c2
Itay Dafna <itaydafna@gmail.com>
2019-07-14 18:20:06 +0300

    delete import

--- src/containers/GameConfig.tsx
+++ src/containers/GameConfig.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { configGameBoard } from '../actions/index';
